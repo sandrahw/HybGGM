26276371aedf5676dc3977783a6d19db8cd0e12e
sandrahw <sandra.hw@hotmail.com>
2024-07-12 14:58:53 +0200

    added example which has more comments in the for loop and played with batchsize to make it run on local laptop

--- src_test/test script.py
+++ src_test/test script.py
@@ -1,92 +1,65 @@
 #test script 
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
 import torch.optim as optim
-
 import torchvision
 import torchvision.transforms as transforms
 
-import matplotlib
-import matplotlib.pyplot as plt
-import numpy as np
-
-
-if torch.cuda.is_available():
-    print("CUDA is available. PyTorch can use the GPU.")
-    print(f"Device count: {torch.cuda.device_count()}")
-    print(f"Device name: {torch.cuda.get_device_name(0)}")
-else:
-    print("CUDA is not available. PyTorch will use the CPU.")
+# Define device
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    
-transform = transforms.Compose(
-    [transforms.ToTensor(), #converstimages loaded by Pillow into PyTorch tensors
-     transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))]) #transforms.Normalize(), adjusts values of tensor so that their average is zero and standrad deviation is 1.0
+print(f"Using device: {device}")
 
-#load CIFAR10 dataset for training
-trainset = torchvision.datasets.CIFAR10(root='./data', train=True,
-                                        download=True, transform=transform)
-trainloader = torch.utils.data.DataLoader(trainset, batch_size=4,
-                                          shuffle=True, num_workers=2)# loads batches of 4 images from trainset, randomizing order, spin upt two worker to load data from disk
-
-# #classes in CIFAR10 dataset and display images
-# classes = ('plane', 'car', 'bird', 'cat',
-#            'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
-
-# def imshow(img):
-#     img = img / 2 + 0.5     # unnormalize
-#     npimg = img.numpy()
-#     plt.imshow(np.transpose(npimg, (1, 2, 0)))
-
-# # get some random training images
-# dataiter = iter(trainloader)
-# images, labels = next(dataiter)
-
-# # show images
-# imshow(torchvision.utils.make_grid(images))
-# # print labels
-# print(' '.join('%5s' % classes[labels[j]] for j in range(4)))
-
-#load CIFAR10 dataset for testing
-testset = torchvision.datasets.CIFAR10(root='./data', train=False,
-                                       download=True, transform=transform)
-testloader = torch.utils.data.DataLoader(testset, batch_size=4,
-                                         shuffle=False, num_workers=2)
-
-# define a Convolutional Neural Network
-class Net(nn.Module):
+# Define the CNN
+class SimpleCNN(nn.Module):
     def __init__(self):
-        super(Net, self).__init__()
-        self.conv1 = nn.Conv2d(3, 6, 5)
+        super(SimpleCNN, self).__init__()
+        self.conv1 = nn.Conv2d(3, 16, 3, padding=1)
+        self.conv2 = nn.Conv2d(16, 32, 3, padding=1)
         self.pool = nn.MaxPool2d(2, 2)
-        self.conv2 = nn.Conv2d(6, 16, 5)
-        self.fc1 = nn.Linear(16 * 5 * 5, 120)
-        self.fc2 = nn.Linear(120, 84)
-        self.fc3 = nn.Linear(84, 10)
+        self.fc1 = nn.Linear(32 * 8 * 8, 128)
+        self.fc2 = nn.Linear(128, 10)
+        self.relu = nn.ReLU()
+        self.softmax = nn.LogSoftmax(dim=1)
 
     def forward(self, x):
-        x = self.pool(F.relu(self.conv1(x)))
-        x = self.pool(F.relu(self.conv2(x)))
-        x = x.view(-1, 16 * 5 * 5)
-        x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
-        x = self.fc3(x)
-        return x    
+        x = self.pool(self.relu(self.conv1(x)))
+        x = self.pool(self.relu(self.conv2(x)))
+        x = x.view(-1, 32 * 8 * 8)
+        x = self.relu(self.fc1(x))
+        x = self.softmax(self.fc2(x))
+        return x
 
-net = Net()
-net.to(device)
+# Load and transform the CIFAR-10 dataset
+transform = transforms.Compose(
+    [transforms.ToTensor(), #converstimages loaded by Pillow into PyTorch tensors
+     transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
 
-# define a Loss function and optimizer
+trainset = torchvision.datasets.CIFAR10(root='./data', train=True,
+                                        download=True, transform=transform)
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=32,
+                                          shuffle=True, num_workers=0)
+
+testset = torchvision.datasets.CIFAR10(root='./data', train=False,
+                                       download=True, transform=transform)
+testloader = torch.utils.data.DataLoader(testset, batch_size=32,
+                                         shuffle=False, num_workers=0)
+
+# Define the loss function and optimizer
+net = SimpleCNN().to(device)
 criterion = nn.CrossEntropyLoss()
 optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
 
-# train the network
-for epoch in range(2):  # loop over the dataset multiple times
+# Train the network
+for epoch in range(10):  # loop over the dataset multiple times
+    print('epoch:', epoch + 1)
     running_loss = 0.0
+    print('start training loop')
     for i, data in enumerate(trainloader, 0):
-        # get the inputs
+        print(f'Batch {i+1}/{len(trainloader)}')
+
+        # get the inputs; data is a list of [inputs, labels]
         inputs, labels = data
+        inputs, labels = inputs.to(device), labels.to(device)
 
         # zero the parameter gradients
         optimizer.zero_grad()
@@ -99,23 +72,22 @@ for epoch in range(2):  # loop over the dataset multiple times
 
         # print statistics
         running_loss += loss.item()
-        if i % 2000 == 1999:    # print every 2000 mini-batches
-            print('[%d, %5d] loss: %.3f' %
-                  (epoch + 1, i + 1, running_loss / 2000))
+        if i % 200 == 199:  # print every 200 mini-batches
+            print(f'[{epoch + 1}, {i + 1}] loss: {running_loss / 200:.3f}')
             running_loss = 0.0
 
 print('Finished Training')
 
-#check for overfitting
+# Test the network on the test data
 correct = 0
 total = 0
 with torch.no_grad():
     for data in testloader:
         images, labels = data
+        images, labels = images.to(device), labels.to(device)
         outputs = net(images)
         _, predicted = torch.max(outputs.data, 1)
         total += labels.size(0)
         correct += (predicted == labels).sum().item()
 
-print('Accuracy of the network on the 10000 test images: %d %%' % (
-    100 * correct / total))
+print(f'Accuracy of the network on the 10000 test images: {100 * correct / total}%')
